4e9a04cc976928b3edda7e113d88ecde1aa75c4d
Rodrigo Vaamonde <vaamonde.rodri@gmail.com>
2024-09-11 19:18:37 +0200

    first commit

---
+++ src/main/java/dev/rodrigovaamonde/demo_modulith/DemoModulithApplication.java
@@ -0,0 +1,13 @@
+package dev.rodrigovaamonde.demo_modulith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoModulithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoModulithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/rodrigovaamonde/demo_modulith/DemoModulithApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rodrigovaamonde.demo_modulith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoModulithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
